aae355f6536f18960fe17b875b98391ac2e612df
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-06-29 22:22:32 +0100

    Without q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Print all the letters except q and e, not followed by a new line."""
 for i in range(97, 123):
     if chr(i) != 'q' and chr(i) != 'e':
         print("{}".format(chr(i)), end="")
