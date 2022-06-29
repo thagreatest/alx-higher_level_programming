3ffaa55e87e94c50a3f25237bafe1299e258f27c
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-06-29 22:16:22 +0100

    Hexadecimal

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+"""Prints all numbers from 0 to 98 in decimal and in hexadecimal"""
 for i in range(0, 99):
-    print("{:d} = 0x{:02x}".format(i, i))
+    print("{:d} = {:02x}".format(i, i))
