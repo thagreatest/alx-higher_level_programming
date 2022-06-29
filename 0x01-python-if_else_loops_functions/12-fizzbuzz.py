bfd607f8928518b8683be646a6393c343eb5fd35
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-06-29 03:28:50 +0100

    Fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print('Fizzbuzz', end="")
+            print('Fizzbuzz ', end="")
         elif i % 3 == 0:
-            print('Fizz', end="")
+            print('Fizz ', end="")
         elif 1 % 5 == 0:
-            print('Buzz', end="")
+            print('Buzz ', end="")
         else:
             print("{}".format(i), end="")
