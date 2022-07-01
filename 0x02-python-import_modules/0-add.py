3a7d235deec5f07c9f8bc168f3823d7d10f4b1f4
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-01 12:59:10 +0100

    Additon importation

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0_add.py
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    import add_it as add
+
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
---
+++ 0x02-python-import_modules/add_it.py
@@ -0,0 +1,3 @@
+def add_it(a, b):
+    """This is the addition function for the add.py function"""
+    return(a + b)
