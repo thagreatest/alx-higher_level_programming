f6db192c7b7ec74be79872984511cb3f61cc20d8
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-05 20:34:10 +0100

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print('{:d}'.format(col), end=' ' if col != row[-1] else '\n')
+            print('{:d}'.format(col), end=' ' if col != row[-1] else '')
         print()
-    print()
-    return matrix
