8f01d43f56b681a17a07b5c99c75ab6a9efb1bfb
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-05 20:24:16 +0100

    Can you c me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_list):
-    new_list = []
-    for i in my_list:
-        if i != 'c' and i != 'C':
-            new_list.append(i)
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if new_list[i] == 'c' or new_list[i] == 'C':
+            new_list[i] = 0
     return new_list
