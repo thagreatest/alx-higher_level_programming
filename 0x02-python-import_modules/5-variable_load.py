b87475aaa49bc792449bebc96639ab10f8cf5bdf
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-05 02:03:05 +0100

    Printing a

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-
-def print_imported_variable():
-    import variable_load_5
-
-    return variable_load_5.a
-
-
 if __name__ == "__main__":
-    print(print_imported_variable())
+    from variable_load_5 import a
+    print("{:d}".format(a))
