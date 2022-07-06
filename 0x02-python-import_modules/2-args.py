52325e55f86d1da75a5b8b3d866bbab50accdb72
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-06 19:12:12 +0100

    Argumentative

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
-if __name__ ==  '__main__':
-
-    count = len(sys.args) - 1
-    if count == 0:
+if __name__ == "__main__":
+    ac = len(argv) - 1
+    if ac == 0:
         print("0 arguments.")
-    elif count == 1:
+    elif ac == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.args[i + 1]))
+        print("{} arguments:".format(ac))
+    for i in range(1, ac + 1):
+        print("{}: {}".format(i, argv[i]))
