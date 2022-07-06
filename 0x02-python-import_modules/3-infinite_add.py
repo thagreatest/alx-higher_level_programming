4339e2f811dce6a01d08d2d8f88e77cf9cc55a6b
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-06 19:16:25 +0100

    INFINTY AND BEYOND

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     sum = 0
     ac = len(argv) - 1
     for i in range(1, ac + 1):
-        sum += int(argv[i])
-    print(sum)
+        sum += int(argv[i + 1])
+    print('{}'.format(sum))
