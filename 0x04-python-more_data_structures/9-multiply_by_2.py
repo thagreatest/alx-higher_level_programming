9c6c6825ce66e423ed632abda6e5340581a76466
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-07 04:08:51 +0100

    Times 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def in_two_places(a_dictionary):
-    return {k: a_dictionary[k] * 2 for k in a_dictionary.items()}
+    return {k: a_dictionary[k] * 2 for (k, value) in a_dictionary.items()}
