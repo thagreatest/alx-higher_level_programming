8fcf9d842d1c287f229d4d1adb46863d2eb18ff5
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-07 04:16:06 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
-    if a_dictionary == {}:
-        return(None)
-    else:
-        return(max(a_dictionary.values()))
+    if (a_dictionary):
+        return(max(a_dictionary, key=a_dictionary.get))
