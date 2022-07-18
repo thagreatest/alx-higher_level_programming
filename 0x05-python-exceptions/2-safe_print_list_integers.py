7deb02eb234fdfbe0d0bd504e82b7b9dae5571d4
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-19 00:49:57 +0100

    printing list integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-def safe_print_list_integer(my_list=[], x=0):
-    printed = 0
-    try:
-        for i in range(0, x):
+def safe_print_list_integers(my_list=[], x=0):
+    item = 0
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
-            printed += 1
-    except (ValueError, TypeError, IndexError):
-        pass
-    
-    
+            item += 1
+        except (ValueError, TypeError):
+            pass
+
     print()
-    return (printed)
+    return item
