467d10a2a64996f361f52360eea82c7c4e2a2984
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-20 00:50:49 +0100

    Creating a single linked list to return the list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,14 +6,13 @@ class Node:
 
     @property
     def data(self):
-        return self.__data
+        return (self.__data)
 
     @data.setter
     def data(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("data must be an integer")
-        else:
-            self.__data = value
+        self.__data = value
 
     @property
     def next_node(self):
@@ -21,41 +20,36 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if value is None or isinstance(value, Node):
-            self.__next_node = value
-        else:
-            raise TypeError("next_node must be a Node object")
+        if (value and type(value) != Node):
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
-        if not self.__head:
-            self.__head = Node(value, self.__head)  # we initialize a new node
-            return None
-        if value < self.__head.data:  # if new data is less, it is new head
-            self.__head = Node(value, self.__head)
-            return None
-        # then if the data is greather
-        new_node = self.__head
-        while new_node.next_node and new_node.next_node.data < value:
-            """ while there is a next node and that data is less, then next """
-            new_node = new_node.next_node
-        new_node.next_node = Node(value, new_node.next_node)
+        new_node = Node(value, None)
+        if (self.__head):
+            aux_node = self.__head
+            while (aux_node):
+                if (new_node.data < aux_node.data):
+                    new_node.next_node = aux_node
+                    aux_node = new_node
+                else:
+                    if (aux_node.next_node):
+                        aux_node = aux_node.next_node
+                    else:
+                        aux_node.next_node = new_node
+        else:
+            self.__head = new_node
+
 
     def __str__(self):
-        if not self.__head:
-            return ""
-        temp = self.__head
-        string = ""
-        while temp is not None:
-            try:
-                string += str(temp.data)
-                temp = temp.next_node
-                string += "\n"
-            except:
-                pass
-        return string[:-1]
+        str_list = ""
+        aux_node = self.__head
+        while (aux_node):
+            str_list += str(aux_node.data) + "\n"
+            aux_node = aux_node.next_node
+        return (str_list)
