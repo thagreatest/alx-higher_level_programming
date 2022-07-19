ae37400d56997b20c56bb8babb34876181921f33
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-20 00:25:25 +0100

    printing the length of a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """
+        Constructor.
+
+        Args:
+            size: the length of a square
+        """
+        self.__size = size
