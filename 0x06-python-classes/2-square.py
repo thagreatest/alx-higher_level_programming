33275fc20d486ee06f7c60143c63500092b8b8a3
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-07-20 00:35:27 +0100

    Value and type error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""square module"""
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """"
+        Constructor.
+
+        Args:
+            size: the length of a square
+
+        Raise:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+        
