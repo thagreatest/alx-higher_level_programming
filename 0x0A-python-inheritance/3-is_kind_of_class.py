5ebdc8f8c21145df1944646604f04bbdf91c4bae
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-08-02 02:11:16 +0100

    Checking if the onject is an instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module 2-is_same_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if obj is instance of a_class"""
+
+    return True if isinstance(obj, a_class) else False
