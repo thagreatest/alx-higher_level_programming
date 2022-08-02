97773415d4de4920f2609802b6f388225c8b6ef9
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-08-03 00:36:42 +0100

    Reads a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """file reader"""
 
+
 def read_file(filename=""):
-    """Reads a file"""
-    
+    """reads a file"""
+
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
