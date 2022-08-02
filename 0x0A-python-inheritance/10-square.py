38b5491f804c67bc3e60095c64bedcfa5d7fb5e5
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-08-02 02:23:16 +0100

    Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""my module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+
+    def __init__(self, size):
+        """constructor"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
