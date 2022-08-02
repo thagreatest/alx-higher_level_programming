ca2432df8fddf3080a96ec60a1201750d2a85c9d
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-08-02 02:16:10 +0100

    Public instance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """does nothing"""
+
+    def area(self):
+        """public instance"""
+        raise Exception("area() is not implemented")
