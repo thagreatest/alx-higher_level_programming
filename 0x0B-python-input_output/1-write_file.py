6593ed1ab9cc0f9a6840d39d49e8b435a7f5089c
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-08-03 00:42:43 +0100

    writes a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyython3
+"""file reader"""
+
+def write_file(filename="", text=""):
+    """writes a file"""
+
+    with open(filename, "w", encoding="utf-8") as file:
+        return (file.write(text))
